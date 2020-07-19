da4e4d90c1849ff6827e01b9693198ce5cef4a73
lorenzorad <lorenzo.radice@gmail.com>
2020-07-19 15:25:39 +0200

    code format

--- public/task1.spec.js
+++ public/task1.spec.js
@@ -1,7 +1,7 @@
 const task1 = require('./task1.js');
 
 describe('task 1', () => {
-    it('returns the readme expected subset', async () => {
+    it('returns the filtered by dates expected subset', async () => {
       const startDate = '2015-08-19T14:00:19.352000Z';
       const endDate = '2015-10-12T07:27:47.493000Z';
 
--- public/task2.spec.js
+++ public/task2.spec.js
@@ -2,24 +2,24 @@ const rawData = require('../src/repository/data.json');
 const task2 = require('./task2.js');
 
 describe('task 2', () => {
-    it('The series (input) does not contains start_date', async () => {
-      const startDate = undefined;
-      const endDate = '2015-10-12T07:27:47.493000Z';
+  it('The series (input) does not contains start_date', async () => {
+    const startDate = undefined;
+    const endDate = '2015-10-12T07:27:47.493000Z';
 
-      const dataToPlot = await task2(startDate, endDate);
+    const dataToPlot = await task2(startDate, endDate);
 
-      const expectedDataToPlot = {
-        data: [
-          {'y': 282, 'x': '2015-08-19T14:00:19.352000Z'},
-          {'y': 227, 'x': '2015-10-08T14:45:31.991000Z'},
-          {'y': 185, 'x': '2015-10-12T07:27:47.493000Z'}
-        ],
-        startDate: undefined,
-        endDate: '2015-10-12T07:27:47.493Z'
-      };
+    const expectedDataToPlot = {
+      data: [
+        {'y': 282, 'x': '2015-08-19T14:00:19.352000Z'},
+        {'y': 227, 'x': '2015-10-08T14:45:31.991000Z'},
+        {'y': 185, 'x': '2015-10-12T07:27:47.493000Z'}
+      ],
+      startDate: undefined,
+      endDate: '2015-10-12T07:27:47.493Z'
+    };
 
-      expect(dataToPlot).toStrictEqual(expectedDataToPlot);
-    });
+    expect(dataToPlot).toStrictEqual(expectedDataToPlot);
+  });
 
   it('The series (input) does not contains end_date', async () => {
     const startDate = '2019-11-19T14:00:19.352000Z';
@@ -30,8 +30,8 @@ describe('task 2', () => {
     const expectedDataToPlot = {
       data: [
         {
-          "x": "2019-11-19T17:14:34.796982Z",
-          "y": 308
+          'x': '2019-11-19T17:14:34.796982Z',
+          'y': 308
         }
       ],
       startDate: '2019-11-19T14:00:19.352Z',
