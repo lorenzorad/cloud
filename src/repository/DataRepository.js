e6e132fc3b4b3fd6b44583db1d471f613ddf7f2a
lorenzorad <lorenzo.radice@gmail.com>
2020-07-19 13:50:03 +0200

    moved data.json file

--- src/repository/DataRepository.js
+++ src/repository/DataRepository.js
@@ -2,7 +2,7 @@ const DomainObjects = require('../domain/DomainObjects.js');
 
 module.exports = class DataRepository {
   async retrieve() { // to simulate and async op eg HTTPS API call
-    const data = require('../../data.json');
+    const data = require('./data.json');
     return new DomainObjects(data);
   }
 }
